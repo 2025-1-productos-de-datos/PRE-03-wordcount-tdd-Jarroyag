2ac7c4bfa51dd6742f5f92464082417ab9f18d5d
Jorge Arroyave <joarroyaveg@unal.edu.co>
2025-05-10 10:32:38 -0500

    homework

--- homework/src/_internals/tests/test_internals.py
+++ homework/src/_internals/tests/test_internals.py
@@ -1,17 +1,37 @@
 import os
 import shutil
+import subprocess
+import sys
 
-from ..count_words import count_words
-from ..preprocess_lines import preprocess_lines
-from ..read_all_lines import read_all_lines
-from ..split_into_words import split_into_words
-from ..write_word_counts import write_word_counts
+from ...wordcount import (
+    count_words,
+    parse_args,
+    prepocess_lines,
+    read_all_lines,
+    split_into_words,
+    write_word_counts,
+)
+
+
+def test_parse_args():
+
+    # Llamada en el prompt:
+    #
+    #   $ python3 -m homework data/input/ data/output/
+    #
+    test_args = ["homework", "data/input/", "data/output/"]
+    sys.argv = test_args
+
+    input_folder, output_folder = parse_args()
+
+    assert input_folder == test_args[1]
+    assert output_folder == test_args[2]
 
 
 def test_read_all_lines():
-    input_folder = "data/input"
+    input_folder = "data/input/"
     lines = read_all_lines(input_folder)
-    assert len(lines) > 0, "No lines were read from the input folder"
+    assert len(lines) > 0
     assert any(
         "Analytics refers to the systematic computational analysis of data" in line
         for line in lines
@@ -20,9 +40,8 @@ def test_read_all_lines():
 
 def test_preprocess_lines():
     lines = ["  Hello, World!  ", "Python is GREAT."]
-    preprocessed = preprocess_lines(lines)
+    preprocessed = prepocess_lines(lines)
     assert preprocessed == ["hello, world!", "python is great."]
-
 
 def test_split_into_words():
     lines = ["hello, world!", "python is great."]
